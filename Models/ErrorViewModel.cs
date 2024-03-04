a550ed98dffd2925f6050e574f60d18e6f6a51b3
VanMo2003 <mjnhhuyen0@gmail.com>
2024-03-04 16:36:52 +0700

    initialize the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace website_shopping.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
